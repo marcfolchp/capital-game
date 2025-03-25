c602d08b20565652326b49ed8a812d01b6cbcc35
Marc Folch <marcfolchpomares@gmail.comm>
2025-03-25 11:51:59 +0100

    game

---
+++ app2.py
@@ -0,0 +1,585 @@
+import streamlit as st
+import pandas as pd
+import random
+import time
+
+# Define file to store users
+CSV_FILE = "user.csv"
+
+# Load user data from CSV
+def load_users():
+    try:
+        return pd.read_csv(CSV_FILE)
+    except (pd.errors.EmptyDataError, FileNotFoundError):
+        return pd.DataFrame(columns=["username", "password", "correct", "total"])
+
+# Save user data to CSV
+def save_users():
+    st.session_state.users_df.to_csv(CSV_FILE, index=False)
+
+# Initialize session state variables if they don't exist
+if "logged_in_user" not in st.session_state:
+    st.session_state.logged_in_user = None  # No user logged in initially
+
+if "users_df" not in st.session_state:
+    st.session_state.users_df = load_users()  # Load user data from CSV
+
+if "page" not in st.session_state:
+    st.session_state.page = "login"  # Default page is login
+
+if "current_question" not in st.session_state:
+    st.session_state.current_question = None
+
+if "options" not in st.session_state:
+    st.session_state.options = []
+
+if "feedback" not in st.session_state:
+    st.session_state.feedback = ""
+
+if "feedback_color" not in st.session_state:
+    st.session_state.feedback_color = ""  # Initialize feedback_color
+
+# Function to generate a random question
+def new_question():
+    countries = {
+    "Afghanistan": "Kabul",
+    "Albania": "Tirana",
+    "Algeria": "Algiers",
+    "Andorra": "Andorra la Vella",
+    "Angola": "Luanda",
+    "Argentina": "Buenos Aires",
+    "Armenia": "Yerevan",
+    "Australia": "Canberra",
+    "Austria": "Vienna",
+    "Azerbaijan": "Baku",
+    "Bahamas": "Nassau",
+    "Bahrain": "Manama",
+    "Bangladesh": "Dhaka",
+    "Barbados": "Bridgetown",
+    "Belarus": "Minsk",
+    "Belgium": "Brussels",
+    "Belize": "Belmopan",
+    "Benin": "Porto-Novo",
+    "Bhutan": "Thimphu",
+    "Bolivia": "Sucre",
+    "Bosnia and Herzegovina": "Sarajevo",
+    "Botswana": "Gaborone",
+    "Brazil": "Brasilia",
+    "Brunei": "Bandar Seri Begawan",
+    "Bulgaria": "Sofia",
+    "Burkina Faso": "Ouagadougou",
+    "Burundi": "Gitega",
+    "Cabo Verde": "Praia",
+    "Cambodia": "Phnom Penh",
+    "Cameroon": "Yaoundé",
+    "Canada": "Ottawa",
+    "Central African Republic": "Bangui",
+    "Chad": "N'Djamena",
+    "Chile": "Santiago",
+    "China": "Beijing",
+    "Colombia": "Bogotá",
+    "Comoros": "Moroni",
+    "Congo (Congo-Brazzaville)": "Brazzaville",
+    "Congo (Congo-Kinshasa)": "Kinshasa",
+    "Costa Rica": "San José",
+    "Croatia": "Zagreb",
+    "Cuba": "Havana",
+    "Cyprus": "Nicosia",
+    "Czechia": "Prague",
+    "Denmark": "Copenhagen",
+    "Djibouti": "Djibouti",
+    "Dominica": "Roseau",
+    "Dominican Republic": "Santo Domingo",
+    "Ecuador": "Quito",
+    "Egypt": "Cairo",
+    "El Salvador": "San Salvador",
+    "Equatorial Guinea": "Malabo",
+    "Eritrea": "Asmara",
+    "Estonia": "Tallinn",
+    "Eswatini": "Mbabane",
+    "Ethiopia": "Addis Ababa",
+    "Fiji": "Suva",
+    "Finland": "Helsinki",
+    "France": "Paris",
+    "Gabon": "Libreville",
+    "Gambia": "Banjul",
+    "Georgia": "Tbilisi",
+    "Germany": "Berlin",
+    "Ghana": "Accra",
+    "Greece": "Athens",
+    "Grenada": "St. George's",
+    "Guatemala": "Guatemala City",
+    "Guinea": "Conakry",
+    "Guinea-Bissau": "Bissau",
+    "Guyana": "Georgetown",
+    "Haiti": "Port-au-Prince",
+    "Honduras": "Tegucigalpa",
+    "Hungary": "Budapest",
+    "Iceland": "Reykjavík",
+    "India": "New Delhi",
+    "Indonesia": "Jakarta",
+    "Iran": "Tehran",
+    "Iraq": "Baghdad",
+    "Ireland": "Dublin",
+    "Israel": "Jerusalem",
+    "Italy": "Rome",
+    "Jamaica": "Kingston",
+    "Japan": "Tokyo",
+    "Jordan": "Amman",
+    "Kazakhstan": "Nur-Sultan",
+    "Kenya": "Nairobi",
+    "Kiribati": "Tarawa",
+    "Kuwait": "Kuwait City",
+    "Kyrgyzstan": "Bishkek",
+    "Laos": "Vientiane",
+    "Latvia": "Riga",
+    "Lebanon": "Beirut",
+    "Lesotho": "Maseru",
+    "Liberia": "Monrovia",
+    "Libya": "Tripoli",
+    "Liechtenstein": "Vaduz",
+    "Lithuania": "Vilnius",
+    "Luxembourg": "Luxembourg",
+    "Madagascar": "Antananarivo",
+    "Malawi": "Lilongwe",
+    "Malaysia": "Kuala Lumpur",
+    "Maldives": "Malé",
+    "Mali": "Bamako",
+    "Malta": "Valletta",
+    "Marshall Islands": "Majuro",
+    "Mauritania": "Nouakchott",
+    "Mauritius": "Port Louis",
+    "Mexico": "Mexico City",
+    "Micronesia": "Palikir",
+    "Moldova": "Chișinău",
+    "Monaco": "Monaco",
+    "Mongolia": "Ulaanbaatar",
+    "Montenegro": "Podgorica",
+    "Morocco": "Rabat",
+    "Mozambique": "Maputo",
+    "Myanmar": "Naypyidaw",
+    "Namibia": "Windhoek",
+    "Nauru": "Yaren",
+    "Nepal": "Kathmandu",
+    "Netherlands": "Amsterdam",
+    "New Zealand": "Wellington",
+    "Nicaragua": "Managua",
+    "Niger": "Niamey",
+    "Nigeria": "Abuja",
+    "North Korea": "Pyongyang",
+    "North Macedonia": "Skopje",
+    "Norway": "Oslo",
+    "Oman": "Muscat",
+    "Pakistan": "Islamabad",
+    "Palau": "Ngerulmud",
+    "Palestine": "Ramallah",
+    "Panama": "Panama City",
+    "Papua New Guinea": "Port Moresby",
+    "Paraguay": "Asunción",
+    "Peru": "Lima",
+    "Philippines": "Manila",
+    "Poland": "Warsaw",
+    "Portugal": "Lisbon",
+    "Qatar": "Doha",
+    "Romania": "Bucharest",
+    "Russia": "Moscow",
+    "Rwanda": "Kigali",
+    "Saint Kitts and Nevis": "Basseterre",
+    "Saint Lucia": "Castries",
+    "Saint Vincent and the Grenadines": "Kingstown",
+    "Samoa": "Apia",
+    "San Marino": "San Marino",
+    "Sao Tome and Principe": "São Tomé",
+    "Saudi Arabia": "Riyadh",
+    "Senegal": "Dakar",
+    "Serbia": "Belgrade",
+    "Seychelles": "Victoria",
+    "Sierra Leone": "Freetown",
+    "Singapore": "Singapore",
+    "Slovakia": "Bratislava",
+    "Slovenia": "Ljubljana",
+    "Solomon Islands": "Honiara",
+    "Somalia": "Mogadishu",
+    "South Africa": "Pretoria",
+    "South Korea": "Seoul",
+    "South Sudan": "Juba",
+    "Spain": "Madrid",
+    "Sri Lanka": "Sri Jayawardenepura Kotte",
+    "Sudan": "Khartoum",
+    "Suriname": "Paramaribo",
+    "Sweden": "Stockholm",
+    "Switzerland": "Bern",
+    "Syria": "Damascus",
+    "Taiwan": "Taipei",
+    "Tajikistan": "Dushanbe",
+    "Tanzania": "Dodoma",
+    "Thailand": "Bangkok",
+    "Togo": "Lomé",
+    "Tonga": "Nuku'alofa",
+    "Trinidad and Tobago": "Port of Spain",
+    "Tunisia": "Tunis",
+    "Turkey": "Ankara",
+    "Turkmenistan": "Ashgabat",
+    "Tuvalu": "Funafuti",
+    "Uganda": "Kampala",
+    "Ukraine": "Kyiv",
+    "United Arab Emirates": "Abu Dhabi",
+    "United Kingdom": "London",
+    "USA": "Washington, D.C.",
+    "Uruguay": "Montevideo",
+    "Uzbekistan": "Tashkent",
+    "Vanuatu": "Port Vila",
+    "Vatican City": "Vatican City",
+    "Venezuela": "Caracas",
+    "Vietnam": "Hanoi",
+    "Yemen": "Sana'a",
+    "Zambia": "Lusaka",
+    "Zimbabwe": "Harare"
+}
+
+    country = random.choice(list(countries.keys()))
+    capital = countries[country]
+
+    options = random.sample(list(countries.values()), 3)  # Select 3 random capitals
+    options.append(capital)  # Add the correct answer to the options
+    random.shuffle(options)  # Shuffle the options
+
+    st.session_state.current_question = country
+    st.session_state.options = options
+
+# Function to check the answer
+def check_answer(selected_option):
+    correct_answer = {
+    "Afghanistan": "Kabul",
+    "Albania": "Tirana",
+    "Algeria": "Algiers",
+    "Andorra": "Andorra la Vella",
+    "Angola": "Luanda",
+    "Argentina": "Buenos Aires",
+    "Armenia": "Yerevan",
+    "Australia": "Canberra",
+    "Austria": "Vienna",
+    "Azerbaijan": "Baku",
+    "Bahamas": "Nassau",
+    "Bahrain": "Manama",
+    "Bangladesh": "Dhaka",
+    "Barbados": "Bridgetown",
+    "Belarus": "Minsk",
+    "Belgium": "Brussels",
+    "Belize": "Belmopan",
+    "Benin": "Porto-Novo",
+    "Bhutan": "Thimphu",
+    "Bolivia": "Sucre",
+    "Bosnia and Herzegovina": "Sarajevo",
+    "Botswana": "Gaborone",
+    "Brazil": "Brasilia",
+    "Brunei": "Bandar Seri Begawan",
+    "Bulgaria": "Sofia",
+    "Burkina Faso": "Ouagadougou",
+    "Burundi": "Gitega",
+    "Cabo Verde": "Praia",
+    "Cambodia": "Phnom Penh",
+    "Cameroon": "Yaoundé",
+    "Canada": "Ottawa",
+    "Central African Republic": "Bangui",
+    "Chad": "N'Djamena",
+    "Chile": "Santiago",
+    "China": "Beijing",
+    "Colombia": "Bogotá",
+    "Comoros": "Moroni",
+    "Congo (Congo-Brazzaville)": "Brazzaville",
+    "Congo (Congo-Kinshasa)": "Kinshasa",
+    "Costa Rica": "San José",
+    "Croatia": "Zagreb",
+    "Cuba": "Havana",
+    "Cyprus": "Nicosia",
+    "Czechia": "Prague",
+    "Denmark": "Copenhagen",
+    "Djibouti": "Djibouti",
+    "Dominica": "Roseau",
+    "Dominican Republic": "Santo Domingo",
+    "Ecuador": "Quito",
+    "Egypt": "Cairo",
+    "El Salvador": "San Salvador",
+    "Equatorial Guinea": "Malabo",
+    "Eritrea": "Asmara",
+    "Estonia": "Tallinn",
+    "Eswatini": "Mbabane",
+    "Ethiopia": "Addis Ababa",
+    "Fiji": "Suva",
+    "Finland": "Helsinki",
+    "France": "Paris",
+    "Gabon": "Libreville",
+    "Gambia": "Banjul",
+    "Georgia": "Tbilisi",
+    "Germany": "Berlin",
+    "Ghana": "Accra",
+    "Greece": "Athens",
+    "Grenada": "St. George's",
+    "Guatemala": "Guatemala City",
+    "Guinea": "Conakry",
+    "Guinea-Bissau": "Bissau",
+    "Guyana": "Georgetown",
+    "Haiti": "Port-au-Prince",
+    "Honduras": "Tegucigalpa",
+    "Hungary": "Budapest",
+    "Iceland": "Reykjavík",
+    "India": "New Delhi",
+    "Indonesia": "Jakarta",
+    "Iran": "Tehran",
+    "Iraq": "Baghdad",
+    "Ireland": "Dublin",
+    "Israel": "Jerusalem",
+    "Italy": "Rome",
+    "Jamaica": "Kingston",
+    "Japan": "Tokyo",
+    "Jordan": "Amman",
+    "Kazakhstan": "Nur-Sultan",
+    "Kenya": "Nairobi",
+    "Kiribati": "Tarawa",
+    "Kuwait": "Kuwait City",
+    "Kyrgyzstan": "Bishkek",
+    "Laos": "Vientiane",
+    "Latvia": "Riga",
+    "Lebanon": "Beirut",
+    "Lesotho": "Maseru",
+    "Liberia": "Monrovia",
+    "Libya": "Tripoli",
+    "Liechtenstein": "Vaduz",
+    "Lithuania": "Vilnius",
+    "Luxembourg": "Luxembourg",
+    "Madagascar": "Antananarivo",
+    "Malawi": "Lilongwe",
+    "Malaysia": "Kuala Lumpur",
+    "Maldives": "Malé",
+    "Mali": "Bamako",
+    "Malta": "Valletta",
+    "Marshall Islands": "Majuro",
+    "Mauritania": "Nouakchott",
+    "Mauritius": "Port Louis",
+    "Mexico": "Mexico City",
+    "Micronesia": "Palikir",
+    "Moldova": "Chișinău",
+    "Monaco": "Monaco",
+    "Mongolia": "Ulaanbaatar",
+    "Montenegro": "Podgorica",
+    "Morocco": "Rabat",
+    "Mozambique": "Maputo",
+    "Myanmar": "Naypyidaw",
+    "Namibia": "Windhoek",
+    "Nauru": "Yaren",
+    "Nepal": "Kathmandu",
+    "Netherlands": "Amsterdam",
+    "New Zealand": "Wellington",
+    "Nicaragua": "Managua",
+    "Niger": "Niamey",
+    "Nigeria": "Abuja",
+    "North Korea": "Pyongyang",
+    "North Macedonia": "Skopje",
+    "Norway": "Oslo",
+    "Oman": "Muscat",
+    "Pakistan": "Islamabad",
+    "Palau": "Ngerulmud",
+    "Palestine": "Ramallah",
+    "Panama": "Panama City",
+    "Papua New Guinea": "Port Moresby",
+    "Paraguay": "Asunción",
+    "Peru": "Lima",
+    "Philippines": "Manila",
+    "Poland": "Warsaw",
+    "Portugal": "Lisbon",
+    "Qatar": "Doha",
+    "Romania": "Bucharest",
+    "Russia": "Moscow",
+    "Rwanda": "Kigali",
+    "Saint Kitts and Nevis": "Basseterre",
+    "Saint Lucia": "Castries",
+    "Saint Vincent and the Grenadines": "Kingstown",
+    "Samoa": "Apia",
+    "San Marino": "San Marino",
+    "Sao Tome and Principe": "São Tomé",
+    "Saudi Arabia": "Riyadh",
+    "Senegal": "Dakar",
+    "Serbia": "Belgrade",
+    "Seychelles": "Victoria",
+    "Sierra Leone": "Freetown",
+    "Singapore": "Singapore",
+    "Slovakia": "Bratislava",
+    "Slovenia": "Ljubljana",
+    "Solomon Islands": "Honiara",
+    "Somalia": "Mogadishu",
+    "South Africa": "Pretoria",
+    "South Korea": "Seoul",
+    "South Sudan": "Juba",
+    "Spain": "Madrid",
+    "Sri Lanka": "Sri Jayawardenepura Kotte",
+    "Sudan": "Khartoum",
+    "Suriname": "Paramaribo",
+    "Sweden": "Stockholm",
+    "Switzerland": "Bern",
+    "Syria": "Damascus",
+    "Taiwan": "Taipei",
+    "Tajikistan": "Dushanbe",
+    "Tanzania": "Dodoma",
+    "Thailand": "Bangkok",
+    "Togo": "Lomé",
+    "Tonga": "Nuku'alofa",
+    "Trinidad and Tobago": "Port of Spain",
+    "Tunisia": "Tunis",
+    "Turkey": "Ankara",
+    "Turkmenistan": "Ashgabat",
+    "Tuvalu": "Funafuti",
+    "Uganda": "Kampala",
+    "Ukraine": "Kyiv",
+    "United Arab Emirates": "Abu Dhabi",
+    "United Kingdom": "London",
+    "USA": "Washington, D.C.",
+    "Uruguay": "Montevideo",
+    "Uzbekistan": "Tashkent",
+    "Vanuatu": "Port Vila",
+    "Vatican City": "Vatican City",
+    "Venezuela": "Caracas",
+    "Vietnam": "Hanoi",
+    "Yemen": "Sana'a",
+    "Zambia": "Lusaka",
+    "Zimbabwe": "Harare"
+}
+
+
+    user_index = st.session_state.users_df.index[
+        st.session_state.users_df["username"] == st.session_state.logged_in_user
+    ].tolist()
+
+    if user_index:  # Ensure user exists in DataFrame
+        user_index = user_index[0]
+
+        if selected_option == correct_answer[st.session_state.current_question]:
+            st.session_state.feedback = "Correct!"
+            st.session_state.feedback_color = "green"
+            st.session_state.users_df.at[user_index, "correct"] += 1
+        else:
+            st.session_state.feedback = "Incorrect!"
+            st.session_state.feedback_color = "red"
+
+        st.session_state.users_df.at[user_index, "total"] += 1
+        save_users()  # Save updated data
+
+    # Generate a new question after answering
+    new_question()
+
+# Display leaderboard
+def leaderboard():
+    users_df = st.session_state.users_df.copy()
+
+    # Ensure columns are numeric before calculation
+    users_df["correct"] = pd.to_numeric(users_df["correct"], errors='coerce').fillna(0)
+    users_df["total"] = pd.to_numeric(users_df["total"], errors='coerce').fillna(1)
+
+    users_df["percentage"] = (users_df["correct"] / users_df["total"] * 100).round(2)
+    leaderboard_df = users_df[["username", "percentage"]].sort_values(by="percentage", ascending=False)
+
+    st.subheader("Leaderboard")
+    st.write(leaderboard_df)
+
+# Game page (question page)
+# Game page (question page)
+def game_page():
+    if st.session_state.current_question is None:
+        new_question()
+
+    st.title("Guess the Capital Game")
+    st.subheader(f"Question: What is the capital of {st.session_state.current_question}?")
+
+    # Show feedback after answer
+    if "feedback" in st.session_state:
+        st.markdown(
+            f"<h4 style='color:{st.session_state.feedback_color};'>{st.session_state.feedback}</h4>",
+            unsafe_allow_html=True,
+        )
+
+    # Display options as buttons with unique keys
+    for idx, option in enumerate(st.session_state.options):
+        button_key = f"{st.session_state.current_question}_{option}_{idx}"  # Ensure unique key
+        if st.button(option, key=button_key):  
+            check_answer(option)  # Check answer immediately
+            st.experimental_rerun()  # Reload page to update without needing an extra click
+
+
+# Register page
+# Register page
+def register_page():
+    st.title("Register")
+    new_username = st.text_input("New Username")
+    new_password = st.text_input("New Password", type="password")
+    confirm_password = st.text_input("Confirm Password", type="password")
+
+    if st.button("Register"):
+        if new_password != confirm_password:
+            st.error("Passwords do not match!")
+        elif new_username in st.session_state.users_df["username"].values:
+            st.error("Username already exists!")
+        else:
+            new_user = pd.DataFrame({
+                "username": [new_username],
+                "password": [new_password],
+                "correct": [0],
+                "total": [0]
+            })
+
+            st.session_state.users_df = pd.concat([st.session_state.users_df, new_user], ignore_index=True)
+            save_users()
+            st.success("Registration successful! Redirecting to login...")
+
+            time.sleep(2)  # Brief pause for user feedback
+            st.session_state.page = "login"
+            st.experimental_rerun()
+
+# Login page
+# Login page
+def login_page():
+    st.title("Login")
+    username = st.text_input("Username").strip()
+    password = st.text_input("Password", type="password").strip()
+
+    col1, col2 = st.columns(2)
+
+    if col1.button("Login"):
+        users_df = st.session_state.users_df
+
+        # Ensure correct data types and strip potential whitespace
+        users_df["username"] = users_df["username"].astype(str).str.strip()
+        users_df["password"] = users_df["password"].astype(str).str.strip()
+
+        user_exists = users_df[users_df["username"] == username]
+
+        if not user_exists.empty and user_exists.iloc[0]["password"] == password:
+            st.session_state.logged_in_user = username
+            st.session_state.page = "game"  # Redirect to game page
+            st.experimental_rerun()  # Ensure proper re-render
+        else:
+            st.error("Invalid username or password")
+
+    if col2.button("Register"):
+        st.session_state.page = "register"
+        st.experimental_rerun()
+
+
+# Main app flow
+def main():
+    if st.session_state.page == "login":
+        login_page()
+    elif st.session_state.page == "register":
+        register_page()
+    elif st.session_state.page == "game" and st.session_state.logged_in_user:
+        # Display game page
+        st.sidebar.title(f"Hello, {st.session_state.logged_in_user}!")
+        menu = ["Questions", "Leaderboard"]
+        page = st.sidebar.radio("Select Page", menu)
+
+        if page == "Questions":
+            game_page()
+        elif page == "Leaderboard":
+            leaderboard()
+
+if __name__ == "__main__":
+    main()
